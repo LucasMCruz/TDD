8a52bd32fb4c66fc0345b9616431d8ac965805d7
lucaslayca <lucasmatheus644@gmail.com>
2020-03-26 14:31:57 -0300

    Tdd modulo 2

---
+++ src/test/java/br/ce/wcaquino/servicos/PedidoTest.java
@@ -0,0 +1,80 @@
+package br.ce.wcaquino.servicos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.servicos.Pedido;
+
+public class PedidoTest {
+	
+	private Pedido pedido;
+	
+	@Before
+	public void setUp() {
+		pedido = new Pedido();
+	
+
+	}
+
+	protected void assertResumoPedido(double valorTotal, double desconto) {
+		ResumoPedido resumoPedido = pedido.resumo();
+		assertEquals(valorTotal,  resumoPedido.getValorTotal(), 0.0001);
+		assertEquals(desconto, resumoPedido.getDesconto(), 0.001);
+	}
+	
+	@Test
+	public void devePermitirAddItem()throws Exception{
+		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 10));
+		
+	}
+	
+	
+	@Test
+	public void deveCalcularValorTotalEDescontoParaPedidoVazio() throws Exception{
+		assertResumoPedido(0.0, 0.0);
+		
+	}
+
+	
+	@Test
+	public void deveCalcularResumoParaItemSemDesconto() throws Exception{
+		pedido.adicionarItem(new ItemPedido("Sabonete", 5.0, 5));
+		assertResumoPedido(25.0, 0.0);
+	}
+	
+	@Test
+	public void deveCalcularResumoParaDoisItensSemDesconto()throws Exception{
+		pedido.adicionarItem(new ItemPedido("Sabonete", 3.0, 3));
+		pedido.adicionarItem(new ItemPedido("Creme Dental", 7.0, 3));
+		
+		assertResumoPedido(30.0, 0.0);
+
+	}
+	
+	@Test
+	public void deveAplicarDescontoNa1aFaixa() throws Exception{
+		pedido.adicionarItem(new ItemPedido("Creme", 20.0, 20));
+		assertResumoPedido(400.0, 16.0);
+	}
+	
+	@Test
+	public void deveAplicarDescontoNa2aFaixa() throws Exception{
+		pedido.adicionarItem(new ItemPedido("Shampoo", 15.0, 30));
+		pedido.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
+		assertResumoPedido(900.0, 54.0);
+		
+	}
+	@Test
+	public void deveAplicarDescontoNa3aFaixa() throws Exception{
+		pedido.adicionarItem(new ItemPedido("Shampoo", 15.0, 30));
+		pedido.adicionarItem(new ItemPedido("Óleo", 15.0, 30));
+		pedido.adicionarItem(new ItemPedido("Sabonete", 10.0, 30));
+		assertResumoPedido(1200.0, 96.0);
+		
+	}
+	
+	
+	
+}
